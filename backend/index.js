2ca73c6d4733acb92d8504531cbf9817218e0858
vmmuthu31 <mvairamuthu2003@gmail.com>
2024-09-30 13:23:42 +0530

    updated the packages

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ const port = process.env.PORT || 8080;
 const dbURI = process.env.MONGODBURI;
 
 mongoose
-  .connect(dbURI)
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err.message));
 
