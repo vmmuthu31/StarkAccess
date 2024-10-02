3757bd80c11951897b2cfa05e4be6d1e3a06346f
Sunil0881 <sunil12162003@gmail.com>
2024-10-02 12:48:48 +0530

    update route

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const transactionSchema = new mongoose.Schema({
   buyer: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   seller: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  ticket: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Ticket",
-    required: true,
-  },
+  ticket: { type: mongoose.Schema.Types.ObjectId, ref: "Ticket", required: true },
   price: { type: Number, required: true },
   transactionDate: { type: Date, default: Date.now },
   status: { type: String, enum: ["completed", "pending"], default: "pending" },
