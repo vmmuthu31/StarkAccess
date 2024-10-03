45ae1cb337b24bbaafcdf96bf3b53703a067d59d
vishalmet <vishalmet003@gmail.com>
2024-10-03 21:38:57 +0530

    adding assets

--- app/components/Assets/Assets.tsx
+++ app/components/Assets/Assets.tsx
@@ -6,6 +6,8 @@ import RingImgs from "@/app/Assets/RingImgs.png";
 import Decor1 from "@/app/Assets/Decor1.png";
 import ArrowRightBlue from "@/app/Assets/arrow-right-blue.png";
 import ArrowRightWhite from "@/app/Assets/arrow-right-white.png";
+import DottedArrowToRight from "@/app/Assets/DottedArrowToRight.png";
+import DottedArrowToLeft from "@/app/Assets/DottedArrowToLeft.png";
 
 const Assets = {
   Logo,
@@ -13,7 +15,9 @@ const Assets = {
   RingImgs,
   Decor1,
   ArrowRightBlue,
-  ArrowRightWhite
+  ArrowRightWhite,
+  DottedArrowToRight,
+  DottedArrowToLeft
 };
 
 export default Assets;
