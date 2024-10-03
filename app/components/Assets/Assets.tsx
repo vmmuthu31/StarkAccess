28584774d000e6f2ed5f7cf14a4ff07df2ddaba5
vishalmet <vishalmet003@gmail.com>
2024-10-03 20:49:44 +0530

    landing page prep with fonts and assets

--- app/components/Assets/Assets.tsx
+++ app/components/Assets/Assets.tsx
@@ -1,11 +1,19 @@
 // Assets.tsx
 import React from 'react';
 import Logo from "@/app/Assets/Logo.png"; 
-import ArrowTopRight from "@/app/Assets/arrow-up-right.png"
+import ArrowTopRight from "@/app/Assets/arrow-up-right.png";
+import RingImgs from "@/app/Assets/RingImgs.png";
+import Decor1 from "@/app/Assets/Decor1.png";
+import ArrowRightBlue from "@/app/Assets/arrow-right-blue.png";
+import ArrowRightWhite from "@/app/Assets/arrow-right-white.png";
 
 const Assets = {
   Logo,
-  ArrowTopRight
+  ArrowTopRight,
+  RingImgs,
+  Decor1,
+  ArrowRightBlue,
+  ArrowRightWhite
 };
 
 export default Assets;
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,13 +8,13 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <div className=" max-w-7xl mx-auto">
+    <div className=" max-w-7xl mx-auto" >
       <div className=" flex items-center justify-between">
         <div className="">
           <Image src={Assets.Logo} alt="Logo" width={200} height={100} />{" "}
           {/* Using Assets.Logo as intended */}
         </div>
-        <div className=" flex items-center gap-[30px]">
+        <div className=" flex items-center gap-[30px] text-xl">
           <h1>Home</h1>
           <h1>Explore Events</h1>
           <h1>Create Events</h1>
