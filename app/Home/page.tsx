6c0142b68f613af9fc6d6680d4d50c0260d0759b
vishalmet <vishalmet003@gmail.com>
2024-10-04 13:05:47 +0530

    landing page completed

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,78 +1,115 @@
+"use client";
 import React from "react";
 import Header from "../components/Header";
 import Assets from "../components/Assets/Assets";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Explore from "./components/Explore";
 
 type Props = {};
 
 const page = (props: Props) => {
   return (
-    <div className=" bg-gradient-to-b from-customStart to-customEnd min-h-screen text-white">
-      <div className="py-3 md:py-6">
-        <Header />
-        <div className=" flex justify-center pt-16">
-          <div className="space-y-3">
-            <div className="flex justify-center items-center text-xl gap-2">
-              <div className="relative">
-                <Image
-                  className=" absolute top-[-20px] left-[-15px] h-6 w-auto z-10" // Add z-index here
-                  src={Assets.Decor1}
-                  alt="Decor"
-                />
+    <div className="min-h-screen text-white">
+      {/* White background extending throughout the page */}
+      <div className="bg-[#EDF3FD] min-h-screen w-full">
+        {/* Section with the background image limited to screen height */}
+        <div className="bg-[url('/BgImg1.webp')] bg-cover bg-center bg-no-repeat">
+          <div className="py-3 md:py-6">
+            <Header />
+            <div className="flex justify-center pt-16">
+              <div className="space-y-3">
+                <div className="flex justify-center items-center text-xl gap-2">
+                  <div className="relative">
+                    <Image
+                      className="absolute top-[-20px] left-[-15px] h-6 w-auto z-10"
+                      src={Assets.Decor1}
+                      alt="Decor"
+                    />
+                    <Image
+                      className="h-[40px] w-auto z-0"
+                      src={Assets.RingImgs}
+                      alt="Ring Images"
+                    />
+                  </div>
+                  <h1 className="text-lg">On-chain events made easy</h1>
+                </div>
+                <div className="text-center text-5xl">
+                  <p>Experience Seamless Connections </p>
+                  <p className="bg-gradient-to-l from-black/10 border-r-4 p-2 border-r-black w-fit mx-auto">
+                    {" "}
+                    at Your Events with Starknet
+                  </p>
+                </div>
+                <h1 className="text-center text-lg">
+                  Experience seamless event management with secure ticketing and
+                  transactions. <br />
+                   Explore, create, and connect like never before!
+                </h1>
 
-                <Image
-                  className="h-[40px] w-auto z-0" // No need for absolute positioning for second image
-                  src={Assets.RingImgs}
-                  alt="Ring Images"
-                />
+                <div className="flex relative justify-center gap-4">
+                  <motion.button
+                    whileTap={{ scale: 0.9 }}
+                    className="text-[#3581F1] gap-1 bg-white p-2 px-3 flex items-center border-4 border-[#78ABFC] rounded-[10px]"
+                  >
+                    Create your Event{" "}
+                    <Image
+                      className="h-6 w-auto"
+                      src={Assets.ArrowRightBlue}
+                      alt="ArrowRight"
+                    ></Image>{" "}
+                  </motion.button>
+                  <motion.button
+                    whileTap={{ scale: 0.9 }}
+                    className="p-2 px-3 gap-1 flex items-center border-2 border-white rounded-[10px]"
+                  >
+                    Create your Event{" "}
+                    <Image
+                      className="h-6 w-auto"
+                      src={Assets.ArrowRightWhite}
+                      alt="ArrowRight"
+                    ></Image>{" "}
+                  </motion.button>
+                </div>
               </div>
+            </div>
 
-              <h1 className=" text-lg">On-chain events made easy</h1>
+            <div className="relative">
+              <Image
+                className="w-1/3"
+                src={Assets.DottedArrowToRight}
+                alt="DottedArrowToRight"
+              ></Image>
+              <Image
+                className="w-1/3 absolute right-0 top-[-200px]"
+                src={Assets.DottedArrowToLeft}
+                alt="DottedArrowToLeft"
+              ></Image>
             </div>
-            <div className=" text-center text-5xl">
-              <p>Experience Seamless Connections </p>
-              <p className=" bg-gradient-to-l from-black/10 border-r-4 p-2 border-r-black w-fit mx-auto">
-                {" "}
-                at Your Events with Starknet
-              </p>
+          </div>
+          <div className="flex justify-center">
+            <div className="relative bg-[#EDF3FD] h-[250px] w-[250px] rounded-[20px] shadow-xl transform rotate-[-8deg]">
+              <Image
+                className="absolute top-[-40px] left-[-40px] h-14 w-auto z-20" // Ensure higher z-index and adjust positioning
+                src={Assets.Decor1}
+                alt="Decor"
+              />
+              <div className=" h-full w-full rounded-[20px] z-10"></div>
             </div>
-            <h1 className=" text-center text-lg">
-              Experience seamless event management with secure ticketing and
-              transactions. <br />
-               Explore, create, and connect like never before!
-            </h1>
 
-            <div className="flex relative justify-center gap-4">
-              <motion.button
-                whileTap={{ scale: 0.9 }}
-                className=" text-[#3581F1] gap-1 bg-white p-2 px-3 flex items-center border-4 border-[#78ABFC] rounded-[10px]"
-              >
-                Create your Event{" "}
-                <Image
-                  className=" h-6 w-auto"
-                  src={Assets.ArrowRightBlue}
-                  alt="ArrowRight"
-                ></Image>{" "}
-              </motion.button>
-              <motion.button
-                whileTap={{ scale: 0.9 }}
-                className="p-2 px-3 gap-1 flex items-center border-2 border-white rounded-[10px]"
-              >
-                Create your Event{" "}
-                <Image
-                  className=" h-6 w-auto"
-                  src={Assets.ArrowRightWhite}
-                  alt="ArrowRight"
-                ></Image>{" "}
-              </motion.button>
+            <div className="relative bg-[#EDF3FD] h-[250px] w-[250px] rounded-[20px] shadow-xl mt-10 transform rotate-[8deg]">
+              <Image
+                className="absolute top-[-40px] right-[-40px] h-14 w-auto z-20" // Ensure higher z-index and adjust positioning
+                src={Assets.Decor2}
+                alt="Decor"
+              />
+              <div className=" h-full w-full rounded-[20px] z-10"></div>
             </div>
-          </div>  
+          </div>
         </div>
-        
-        <div className="">
-          <Image className=" w-1/3" src={Assets.DottedArrowToRight} alt="DottedArrowToRight"></Image>
-          <Image className=" w-1/3 absolute right-0 top-[200px]" src={Assets.DottedArrowToLeft} alt="DottedArrowToLeft"></Image>
+
+        <div className="pt-10">
+          <Explore />
         </div>
       </div>
     </div>
--- app/components/Assets/Assets.tsx
+++ app/components/Assets/Assets.tsx
@@ -1,23 +1,31 @@
 // Assets.tsx
 import React from 'react';
 import Logo from "@/app/Assets/Logo.png"; 
+import Logo1 from "@/app/Assets/Logo1.png"; 
 import ArrowTopRight from "@/app/Assets/arrow-up-right.png";
 import RingImgs from "@/app/Assets/RingImgs.png";
 import Decor1 from "@/app/Assets/Decor1.png";
+import Decor2 from "@/app/Assets/Decor2.png";
 import ArrowRightBlue from "@/app/Assets/arrow-right-blue.png";
 import ArrowRightWhite from "@/app/Assets/arrow-right-white.png";
 import DottedArrowToRight from "@/app/Assets/DottedArrowToRight.png";
 import DottedArrowToLeft from "@/app/Assets/DottedArrowToLeft.png";
+import Login from "@/app/Assets/login.png";
+import Google from "@/app/Assets/Google.png"
 
 const Assets = {
   Logo,
+  Logo1,
   ArrowTopRight,
   RingImgs,
   Decor1,
+  Decor2,
   ArrowRightBlue,
   ArrowRightWhite,
   DottedArrowToRight,
-  DottedArrowToLeft
+  DottedArrowToLeft,
+  Login,
+  Google
 };
 
 export default Assets;
