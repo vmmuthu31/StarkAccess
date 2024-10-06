aeff7ff6388a2e87f4d2380b41724ab97d3bafd9
vishalmet <vishalmet003@gmail.com>
2024-10-06 18:49:06 +0530

    made pages responsive

--- app/Home/components/Explore.tsx
+++ app/Home/components/Explore.tsx
@@ -16,7 +16,7 @@ const Explore = (props: Props) => {
     <div className="max-w-5xl mx-auto text-black">
       <div>
         <p className="text-2xl text-center">Explore Events</p>
-        <div className="grid grid-cols-3 py-6">
+        <div className="md:grid md:grid-cols-3 py-6 gap-6 space-y-3 md:space-y-0">
           {events.map((event) => (
             <EventCard key={event.id} event={event} />
           ))}
