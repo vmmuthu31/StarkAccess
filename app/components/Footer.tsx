3957538e25c80e85197c82d2f832cb06e37f5b61
vishalmet <vishalmet003@gmail.com>
2024-10-07 20:17:42 +0530

    dynamic footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,11 +9,11 @@ type Props = {}
 const Footer = (props: Props) => {
   return (
     <div className=' max-w-5xl flex justify-center items-center mx-auto'>
-        <div className=" border-t-2 border-[#4390F2] w-full mx-3 md:mx-0">
-            <div className=" pt-2 pb-6 md:flex justify-between items-center">
+        <div className=" border-t-2 border-[#4390F2] w-full mx-3 md:mx-6 lg:mx-0">
+            <div className=" pt-2 pb-6 lg:flex justify-between items-center">
               <div className=" flex justify-between">
                 <motion.p whileTap={{ scale : 0.9 }} className=' bricolage-font text-[#4390F2] font-semibold text-xl hover:cursor-pointer'>StarkAccess</motion.p>
-                <div className="items-center gap-3 flex">
+                <div className="items-center gap-3 flex lg:hidden">
                   <Image className=' h-7 w-auto hover:cursor-pointer' src={Assets.Twitter} alt='Twitter'></Image>
                   <Image className=' h-7 w-auto hover:cursor-pointer' src={Assets.LinkedIn} alt='LinkedIn'></Image>
                 </div>
@@ -24,7 +24,7 @@ const Footer = (props: Props) => {
                   <motion.p whileTap={{ scale : 0.9 }} className=' flex items-center gap-1 hover:cursor-pointer'> <Image className=' h-6 w-auto' src={Assets.Manage} alt="Manage tickets"/> Manage Tickets</motion.p>
                   <motion.p whileTap={{ scale : 0.9 }} className=' flex items-center gap-1 hover:cursor-pointer'> <Image className=' h-6 w-auto' src={Assets.Verify} alt="Verify event"/> Verify Tickets (zkSNARKsProof)</motion.p>
                 </div>
-                <div className="items-center gap-5 hidden md:flex">
+                <div className="items-center gap-5 hidden lg:flex">
                   <Image className=' h-10 w-auto hover:cursor-pointer' src={Assets.Twitter} alt='Twitter'></Image>
                   <Image className=' h-10 w-auto hover:cursor-pointer' src={Assets.LinkedIn} alt='LinkedIn'></Image>
                 </div>
